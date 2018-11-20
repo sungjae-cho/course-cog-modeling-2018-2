6e0f414c59ed47ffdb8fc69b7816b5b6094a177b
Sungjae Cho <sungjae.cho.1118@gmail.com>
2018-11-20 16:15:13 +0900

    Added the textbook.

--- asmt_9.py
+++ asmt_9.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Assignment 9: Implement the prototype model in the textbook (p.16-23)
+Textbook: Busemeyer, J. R., & Diederich, A. (2010). Cognitive Modeling. SAGE.
 Course: The Fundamentals and Applications of Cognitive Modeling
 Author: Sungjae Cho
 """
@@ -14,31 +15,31 @@ c = 1
 beta = 1
 
 def main():
-    
+
     stimuli = np.array([[3.], [7.]])
     category_feedbacks = np.array([[0.], [1.]])
-    
+
     X_detectors = [0., 6., 12.]
     Y_detectors = [0., 6., 12.]
     ideal_points = np.array([X_detectors, Y_detectors])
-    
-    weights = np.array([[1, 0.02, 0.01, 0.02, 0.01, 1], 
+
+    weights = np.array([[1, 0.02, 0.01, 0.02, 0.01, 1],
                        [0.02, 1, 0.01, 0.01, 1, 0.02]])
-    
+
     activations = get_activations(stimuli, ideal_points)
     category_prob, category_activations = get_category_prob(activations, weights)
-    
+
     print("Activations:")
     print(activations)
-    
+
     print("Category probability:")
     print(category_prob)
-    
+
     print("Weights before the delta rule update:")
     print(weights)
-    
+
     weights = delta_rule_update(weights, category_feedbacks, category_activations, activations)
-    
+
     print("Weights after the delta rule update:")
     print(weights)
     print("If every wieght decreased, the implementation is correct.")
@@ -49,7 +50,7 @@ def get_similarity(stimulus, detector):
 def get_activations(stimuli, ideal_points):
     similarity = get_similarity(stimuli, ideal_points)
     return similarity / np.sum(similarity, axis=1, keepdims=True)
-    
+
 def get_category_prob(activations, weights):
     activations = activations.reshape((-1,1))
     category_activations = np.matmul(weights, activations)
@@ -59,7 +60,7 @@ def get_category_prob(activations, weights):
     return category_prob, category_activations
 
 def delta_rule_update(weights, category_feedbacks, category_activations, activations):
-    weights = weights + alpha * (category_feedbacks - category_activations) * activations.reshape((1,-1)) 
+    weights = weights + alpha * (category_feedbacks - category_activations) * activations.reshape((1,-1))
     return weights
 
 if __name__ == "__main__":
